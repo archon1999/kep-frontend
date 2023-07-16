4f49250ff1ca34d9a6ca8ef8ff34c8b54fe8c1de
nboltaboyev <n.boltaboyev@uzinfocom.uz>
2023-07-16 15:02:55 +0500

    add usersChartCard component

---
+++ src/app/shared/components/users-chart/users-chart.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsersChartCardComponent } from './users-chart-card/users-chart-card.component';
+import { CorePipesModule } from '../../../../@core/pipes/pipes.module';
+import { UserPopoverModule } from '../user-popover/user-popover.module';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { CoreDirectivesModule } from '../../../../@core/directives/directives';
+import { TranslateModule } from '@ngx-translate/core';
+
+@NgModule({
+  declarations: [
+    UsersChartCardComponent
+  ],
+  imports: [
+    CommonModule,
+    CorePipesModule,
+    CoreDirectivesModule,
+    TranslateModule,
+    UserPopoverModule,
+    NgApexchartsModule
+  ],
+  exports: [
+    UsersChartCardComponent
+  ]
+})
+export class UsersChartModule { }
