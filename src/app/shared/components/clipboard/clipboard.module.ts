4f8df1b8049d658803d1cbe2a772bd73966b00ea
nboltaboyev <n.boltaboyev@uzinfocom.uz>
2023-07-16 16:27:33 +0500

    fix bug third-part-modules css

--- src/app/shared/components/clipboard/clipboard.module.ts
+++ src/app/shared/components/clipboard/clipboard.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClipboardButtonComponent } from './clipboard-button/clipboard-button.component';
-import { ToastrModule } from 'ngx-toastr';
+import { ToastrModule } from 'app/shared/third-part-modules/toastr/toastr.module';
 import { CoreDirectivesModule } from '../../../../@core/directives/directives';
 import { TranslateModule } from '@ngx-translate/core';
 
