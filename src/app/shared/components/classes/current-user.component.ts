073178192926654409f652fe406ec57015867a49
nboltaboyev <n.boltaboyev@uzinfocom.uz>
2023-08-27 18:58:14 +0500

    refactoring

--- src/app/shared/components/classes/current-user.component.ts
+++ src/app/shared/components/classes/current-user.component.ts
@@ -2,21 +2,23 @@ import { Component, OnDestroy, OnInit } from "@angular/core";
 import { User } from "app/auth/models";
 import { AuthenticationService } from "app/auth/service";
 import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 
 @Component({
   template: '',
 })
 export class CurrentUser implements OnInit, OnDestroy {
-  
+
   public currentUser: User | null;
-  private _unsubscribeAll = new Subject();
+
+  protected _unsubscribeAll = new Subject();
 
   constructor(
     public authService: AuthenticationService,
   ) { }
 
   ngOnInit(): void {
-    this.authService.currentUser.subscribe(
+    this.authService.currentUser.pipe(takeUntil(this._unsubscribeAll)).subscribe(
       (user: User | null) => {
         this.currentUser = user;
       }
