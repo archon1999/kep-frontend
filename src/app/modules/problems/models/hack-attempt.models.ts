45ff6da51505bc613ae4955d2698f49cca0b8740
archon1999 <archon1999@github.com>
2023-09-11 08:28:18 +0500

    hack attempt init

---
+++ src/app/modules/problems/models/hack-attempt.models.ts
@@ -0,0 +1,12 @@
+export class HackAttempt {
+  id: number;
+  status: number;
+  statusTitle: string;
+  created: string;
+}
+
+export interface WSHackAttempt {
+  id: number;
+  status: number;
+  statusTitle: string;
+}
