f78138bc37504a7c3ba658e813b581179a2dfdd5
nboltaboyev <n.boltaboyev@uzinfocom.uz>
2023-09-24 22:40:48 +0500

    add calendar to home page

---
+++ src/app/modules/home/calendar-section/calendar-section.component.ts
@@ -0,0 +1,113 @@
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import interactionPlugin from '@fullcalendar/interaction';
+import { FullCalendarComponent } from '@fullcalendar/angular';
+
+import uzLocale from '@fullcalendar/core/locales/uz';
+import ruLocale from '@fullcalendar/core/locales/ru';
+import enLocale from '@fullcalendar/core/locales/es-us';
+import { TranslateService } from '@ngx-translate/core';
+import { ApiService } from '../../../shared/services/api.service';
+
+enum CalendarEventType {
+  CONTEST = 1,
+  ARENA = 2,
+  TOURNAMENT = 3,
+  HOLIDAY = 4,
+}
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'calendar-section',
+  templateUrl: './calendar-section.component.html',
+  styleUrls: ['./calendar-section.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class CalendarSectionComponent implements OnInit {
+
+  public calendarRef = [
+    {
+      id: 1,
+      checked: true,
+      filter: this.translateService.instant('Contests'),
+      color: 'primary',
+    },
+    {
+      id: 2,
+      checked: true,
+      filter: this.translateService.instant('Arena'),
+      color: 'warning',
+    },
+    {
+      id: 3,
+      checked: true,
+      filter: this.translateService.instant('Tournaments'),
+      color: 'dark',
+    },
+    {
+      id: 4,
+      checked: true,
+      filter: this.translateService.instant('Holidays'),
+      color: 'success',
+    },
+  ];
+
+  @ViewChild('calendar') calendarComponent: FullCalendarComponent;
+
+  calendarOptions: CalendarOptions = {
+    plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
+    headerToolbar: {
+      start: 'sidebarToggle, prev,next, title',
+      end: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+    },
+    locale: this.translateService.currentLang,
+    locales: [uzLocale, ruLocale, enLocale],
+    initialView: 'dayGridMonth',
+    weekends: true,
+    editable: false,
+    eventResizableFromStart: true,
+    selectable: false,
+    selectMirror: true,
+    dayMaxEvents: 2,
+    navLinks: true,
+    datesSet: (dateInfo) => {
+      console.log(dateInfo);
+    }
+  };
+
+  constructor(public translateService: TranslateService, public api: ApiService) {
+  }
+
+  ngOnInit(): void {
+    this.api.get('calendar-events').subscribe(
+      (events: Array<any>) => {
+        this.calendarOptions.events = events.map((event) => {
+          event.start = new Date(event.startTime);
+          event.end = new Date(event.finishTime);
+          switch (event.type) {
+            case CalendarEventType.CONTEST:
+              event.url = `/competitions/contests/contest/${event.uid}`;
+              event.className = 'bg-light-primary';
+              break;
+            case CalendarEventType.ARENA:
+              event.url = `/competitions/arena/tournament/${event.uid}`;
+              event.className = 'bg-light-warning';
+              break;
+            case CalendarEventType.TOURNAMENT:
+              event.url = `/competitions/tournaments/tournament/${event.uid}`;
+              event.className = 'bg-light-dark';
+              break;
+            case CalendarEventType.HOLIDAY:
+              event.className = 'bg-light-success';
+              break;
+          }
+          return event;
+        });
+      }
+    );
+  }
+
+}
