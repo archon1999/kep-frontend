f500d228798a7344c5ba12773fa6c36de695e83a
nboltaboyev <n.boltaboyev@uzinfocom.uz>
2023-09-24 12:50:25 +0500

    clean

--- src/app/shared/c-validators/het-validators/validators/required/required.config.ts
+++
@@ -1,8 +0,0 @@
-import { BaseConfig } from "../../basic/base-config";
-
-export interface RequiredConfig extends BaseConfig {}
-
-export const requiredDefaultConfig: RequiredConfig = {
-  errorName: 'requiredError',
-  errorTranslateMessage: 'REQUIRED_ERROR',
-}
--- src/app/shared/c-validators/het-validators/validators/required/required.validator.ts
+++
@@ -1,8 +0,0 @@
-import { FormControl, ValidatorFn } from "@angular/forms";
-import { RequiredConfig } from "./required.config";
-
-export function requiredValidator(config?: RequiredConfig): ValidatorFn {
-  return (control: FormControl) => {
-    if(control.value === null) return {}
-  }
-}
