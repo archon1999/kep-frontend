3b870b07c9f689504a64837bdea508dbefa5eb7a
nboltaboyev <n.boltaboyev@uzinfocom.uz>
2023-09-30 19:12:16 +0500

    refactoring currentUser component

--- src/app/shared/components/classes/current-user.component.ts
+++ src/app/shared/components/classes/current-user.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
-import { User } from "app/auth/models";
-import { AuthenticationService } from "app/auth/service";
-import { Subject } from "rxjs";
-import { takeUntil } from "rxjs/operators";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { User } from 'app/auth/models';
+import { AuthenticationService } from 'app/auth/service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   template: '',
 })
+// tslint:disable-next-line:component-class-suffix
 export class CurrentUser implements OnInit, OnDestroy {
 
   public currentUser: User | null;
@@ -15,19 +16,25 @@ export class CurrentUser implements OnInit, OnDestroy {
 
   constructor(
     public authService: AuthenticationService,
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
     this.authService.currentUser.pipe(takeUntil(this._unsubscribeAll)).subscribe(
       (user: User | null) => {
+        console.log(user);
         this.currentUser = user;
       }
-    )
+    );
+  }
+
+  isAuthenticated() {
+    return this.currentUser !== null;
   }
 
   ngOnDestroy(): void {
-    this._unsubscribeAll.next();    
-    this._unsubscribeAll.complete();    
+    this._unsubscribeAll.next();
+    this._unsubscribeAll.complete();
   }
 
 }
