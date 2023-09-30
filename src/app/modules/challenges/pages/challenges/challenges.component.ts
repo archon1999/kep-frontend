6848050a3e800d02cc689ebe9f6f3627525f7ddc
nboltaboyev <n.boltaboyev@uzinfocom.uz>
2023-09-30 12:42:08 +0500

    refactoring chapter colors && add select chapters to new-challenge form

--- src/app/modules/challenges/pages/challenges/challenges.component.ts
+++ src/app/modules/challenges/pages/challenges/challenges.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { fadeInLeftAnimation, fadeInLeftOnEnterAnimation, fadeInOnEnterAnimation, fadeInRightAnimation, fadeInRightOnEnterAnimation, fadeInUpAnimation, fadeInUpOnEnterAnimation, fadeOutOnLeaveAnimation } from 'angular-animations';
+import {
+  fadeInLeftOnEnterAnimation,
+  fadeInOnEnterAnimation,
+  fadeInRightOnEnterAnimation,
+  fadeInUpOnEnterAnimation,
+  fadeOutOnLeaveAnimation
+} from 'angular-animations';
 import { AuthenticationService } from '../../../../auth/service';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { User } from '../../../users/users.models';
 import { Challenge, ChallengeCall, ChallengesRating } from '../../models/challenges.models';
 import { ChallengesService } from '../../services/challenges.service';
+import { Chapter } from '../../../testing/testing.models';
 
 interface NewChallengeCall {
   timeSeconds: number;
@@ -29,12 +36,14 @@ export class ChallengesComponent implements OnInit, OnDestroy {
   public challengesRating: Array<ChallengesRating> = [];
   public challengeCalls: Array<ChallengeCall> = [];
   public challenges: Array<Challenge> = [];
-  public myChallenges: boolean = false;
+  public myChallenges = false;
 
+  public chapters: Chapter[] = [];
+  public selectedChapters = [];
   public newChallengeCall: NewChallengeCall = {
     timeSeconds: 40,
     questionsCount: 6,
-  }
+  };
 
   public currentUser: User;
 
@@ -45,89 +54,100 @@ export class ChallengesComponent implements OnInit, OnDestroy {
     public service: ChallengesService,
     public authService: AuthenticationService,
     public router: Router,
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
     this.authService.currentUser.pipe(takeUntil(this._unsubscribeAll)).subscribe(
       (user: any) => {
         this.currentUser = user;
       }
-    )
+    );
+
+    this.service.getChapters().subscribe(
+      (chapters: Array<Chapter>) => {
+        this.chapters = chapters;
+      }
+    );
 
     this.service.getChallengesRating(1).subscribe(
       (result: any) => {
         this.challengesRating = result.data;
       }
-    )
+    );
 
     this.updateChallengeCalls();
-    this._intervalId = setInterval(() => this.updateChallengeCalls(), 5000)
+    this._intervalId = setInterval(() => this.updateChallengeCalls(), 5000);
 
     this.updateChallenges();
   }
 
-  updateChallenges(){
+  updateChallenges() {
     let username = null;
-    if(this.myChallenges){
+    if (this.myChallenges) {
       username = this.currentUser?.username;
     }
     this.service.getChallenges(1, username).subscribe(
       (result: any) => {
         this.challenges = result.data;
       }
-    )
+    );
   }
 
-  updateChallengeCalls(){
+  updateChallengeCalls() {
     this.service.getChallengeCalls().subscribe(
       (challengeCalls: Array<ChallengeCall>) => {
-        for(let i = 0; i < this.challengeCalls.length; i++){
-          if(!challengeCalls.find((value: ChallengeCall) => value.id == this.challengeCalls[i].id)){
+        for (let i = 0; i < this.challengeCalls.length; i++) {
+          if (!challengeCalls.find((value: ChallengeCall) => value.id === this.challengeCalls[i].id)) {
             this.challengeCalls.splice(i, 1);
           }
         }
-        for(let challengeCall of challengeCalls){
-          if(!this.challengeCalls.find((value: ChallengeCall) => value.id == challengeCall.id)){
+        for (const challengeCall of challengeCalls) {
+          if (!this.challengeCalls.find((value: ChallengeCall) => value.id === challengeCall.id)) {
             this.challengeCalls.push(challengeCall);
           }
         }
       }
-    )
+    );
   }
 
-  newChallenge(){
-    this.service.newChallengeCall(this.newChallengeCall.timeSeconds, this.newChallengeCall.questionsCount).subscribe(
+  newChallenge() {
+    this.service.newChallengeCall(
+      this.newChallengeCall.timeSeconds,
+      this.newChallengeCall.questionsCount,
+      this.selectedChapters,
+    ).subscribe(
       (result: any) => {
-        if(result.success){
+        if (result.success) {
           this.updateChallengeCalls();
         }
       }
-    )
+    );
   }
 
-  onChallengeCallDelete(challengeCallId: number){
-    for(let i = 0; i < this.challengeCalls.length; i++){
-      if(this.challengeCalls[i].id == challengeCallId){
+  onChallengeCallDelete(challengeCallId: number) {
+    for (let i = 0; i < this.challengeCalls.length; i++) {
+      if (this.challengeCalls[i].id === challengeCallId) {
         this.challengeCalls.splice(i, 1);
       }
     }
   }
 
-  acceptChallengeCall(challengeCallId: number){
+  acceptChallengeCall(challengeCallId: number) {
     this.service.acceptChallengeCall(challengeCallId).subscribe(
       (result: any) => {
-        if(result.success){
-          let challengeId = result.challengeId;
+        if (result.success) {
+          const challengeId = result.challengeId;
           this.router.navigate(['/practice', 'challenges', 'challenge', challengeId]);
         }
       }
-    )
+    );
   }
 
-  quickStart(timeSeconds: number, questionsCount: number){
-    for(let challengeCall of this.challengeCalls){
-      if(challengeCall.timeSeconds == timeSeconds && challengeCall.questionsCount == questionsCount){
-        if(challengeCall.username != this.currentUser.username){
+  quickStart(timeSeconds: number, questionsCount: number) {
+    for (const challengeCall of this.challengeCalls) {
+      if (challengeCall.timeSeconds === timeSeconds && challengeCall.questionsCount === questionsCount) {
+        if (challengeCall.username !== this.currentUser.username) {
           this.acceptChallengeCall(challengeCall.id);
           break;
         }
@@ -136,7 +156,7 @@ export class ChallengesComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    if(this._intervalId){
+    if (this._intervalId) {
       clearInterval(this._intervalId);
     }
     this._unsubscribeAll.next();
