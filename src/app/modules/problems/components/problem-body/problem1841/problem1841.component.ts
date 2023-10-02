726cee0d5aabfc3cbe1216dd3ab8aefa565c0be6
archon1999 <nazarbek.baltabaev@gmail.com>
2023-10-02 16:13:22 +0500

    refactoring html problems && add new templates for 1843 & 1842

--- src/app/modules/problems/components/problem-body/problem1841/problem1841.component.ts
+++ src/app/modules/problems/components/problem-body/problem1841/problem1841.component.ts
@@ -3,12 +3,12 @@ import { Problem } from '../../../models/problems.models';
 
 @Component({
   // tslint:disable-next-line:component-selector
-  selector: 'problem1740',
-  templateUrl: './problem1740.component.html',
-  styleUrls: ['./problem1740.component.scss'],
+  selector: 'problem1841',
+  templateUrl: './problem1841.component.html',
+  styleUrls: ['./problem1841.component.scss'],
   encapsulation: ViewEncapsulation.Emulated,
 })
-export class Problem1740Component implements OnInit {
+export class Problem1841Component implements OnInit {
 
   @Input() problem: Problem;
 
