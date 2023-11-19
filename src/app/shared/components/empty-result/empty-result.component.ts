2f0e89db6df6ad9450f6b3e5cff5491b48132899
archon1999 <nazarbek.baltabaev@gmail.com>
2023-11-19 16:03:16 +0500

    add empty-result component

---
+++ src/app/shared/components/empty-result/empty-result.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'empty-result',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './empty-result.component.html',
+  styleUrl: './empty-result.component.scss'
+})
+export class EmptyResultComponent {
+
+}
