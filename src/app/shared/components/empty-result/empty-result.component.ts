e1ce0c774f5cdbfa7febc0edee9b387e84c023a0
archon1999 <nazarbek.baltabaev@gmail.com>
2023-11-22 00:22:49 +0500

    empty-result component refactoring

--- src/app/shared/components/empty-result/empty-result.component.ts
+++ src/app/shared/components/empty-result/empty-result.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CoreConfigService } from '../../../../core/services/config.service';
+import { Subject } from 'rxjs';
+import { GlobalService } from '@shared/services/global.service';
 
 @Component({
   selector: 'empty-result',
@@ -8,6 +11,24 @@ import { CommonModule } from '@angular/common';
   templateUrl: './empty-result.component.html',
   styleUrl: './empty-result.component.scss'
 })
-export class EmptyResultComponent {
+export class EmptyResultComponent implements OnInit {
+  @Input() imgHeight: string;
+  @Input() imgWidth: string;
+
+  public imgSrc: string;
+
+  constructor(public globalService: GlobalService) {}
+
+  ngOnInit() {
+    this.globalService.coreConfig$.subscribe(
+      (config) => {
+        if (config.layout.skin === 'dark') {
+          this.imgSrc = 'assets/images/elements/search-not-found-dark.png';
+        } else {
+          this.imgSrc = 'assets/images/elements/search-not-found-light.png';
+        }
+      }
+    );
+  }
 
 }
