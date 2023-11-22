35789df0719ec19c8e28b26717c8c80b230a858b
nboltaboyev <n.boltaboyev@uzinfocom.uz>
2023-11-22 18:38:28 +0500

    make home page lazy loading

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,10 +1,39 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { SoundsService } from '@shared/services/sounds/sounds.service';
+import { CommonModule } from '@angular/common';
+import { PostsSectionComponent } from './posts-section/posts-section.component';
+import { BirthdaysSectionComponent } from './birthdays-section/birthdays-section.component';
+import { CalendarSectionComponent } from './calendar-section/calendar-section.component';
+import { TopRatingSectionComponent } from './top-rating-section/top-rating-section.component';
+import { SystemSectionComponent } from './system-section/system-section.component';
+import { StatisticsSectionComponent } from './statistics-section/statistics-section.component';
+import { NewsSectionComponent } from './news-section/news-section.component';
+import { ProfileSectionComponent } from './profile-section/profile-section.component';
+import { HeaderSectionComponent } from './header-section/header-section.component';
+import { ActivitySectionComponent } from './activity-section/activity-section.component';
+import { UsersChartModule } from '@shared/components/users-chart/users-chart.module';
+import { SpinnerComponent } from '@shared/components/spinner/spinner.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    PostsSectionComponent,
+    BirthdaysSectionComponent,
+    CalendarSectionComponent,
+    TopRatingSectionComponent,
+    SystemSectionComponent,
+    StatisticsSectionComponent,
+    NewsSectionComponent,
+    ProfileSectionComponent,
+    HeaderSectionComponent,
+    ActivitySectionComponent,
+    UsersChartModule,
+    SpinnerComponent,
+  ]
 })
 export class HomeComponent implements OnInit {
   @ViewChild('audio') audio: ElementRef<HTMLAudioElement>;
