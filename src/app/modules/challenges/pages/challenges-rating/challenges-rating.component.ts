634d3557154083d555af1d5ca6c28a94ef1d7a02
archon1999 <archon1999@github.com>
2023-11-30 21:17:25 +0500

    [REFACTORING] challenges-rating page

--- src/app/modules/challenges/pages/challenges-rating/challenges-rating.component.ts
+++ src/app/modules/challenges/pages/challenges-rating/challenges-rating.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { ChallengesRating } from '../../models/challenges.models';
-import { ChallengesService } from '../../services/challenges.service';
-import { PageResult } from '../../../../shared/page-result';
-import { ContentHeader } from '../../../../layout/components/content-header/content-header.component';
+import { ChallengesRating } from '@challenges/models/challenges.models';
+import { ChallengesService } from '@challenges/services';
+import { PageResult } from '@shared/page-result';
+import { ContentHeader } from '@layout/components/content-header/content-header.component';
+import { BaseTablePageComponent } from '@shared/components/classes/base-table-page.component';
+import { Observable } from 'rxjs';
+import { CoreCommonModule } from '@core/common.module';
+import { ChallengesUserViewModule } from '@challenges/components/challenges-user-view/challenges-user-view.module';
+import { KepPaginationComponent } from '@shared/components/pagination/kep-pagination.component';
+import { ContentHeaderModule } from '@layout/components/content-header/content-header.module';
+import { SpinnerComponent } from '@shared/components/spinner/spinner.component';
+import { KepTableComponent } from '@shared/components/kep-table/kep-table.component';
 
 @Component({
   selector: 'app-challenges-rating',
   templateUrl: './challenges-rating.component.html',
-  styleUrls: ['./challenges-rating.component.scss']
+  styleUrls: ['./challenges-rating.component.scss'],
+  standalone: true,
+  imports: [
+    CoreCommonModule,
+    ChallengesUserViewModule,
+    KepPaginationComponent,
+    ContentHeaderModule,
+    SpinnerComponent,
+    KepTableComponent,
+  ]
 })
-export class ChallengesRatingComponent implements OnInit {
+export class ChallengesRatingComponent extends BaseTablePageComponent<ChallengesRating> implements OnInit {
+  override pageNumber = 1;
+  override pageSize = 20;
+  override maxSize = 5;
 
   public contentHeader: ContentHeader = {
     headerTitle: 'Rating',
@@ -26,24 +46,20 @@ export class ChallengesRatingComponent implements OnInit {
     }
   };
 
-  public challengesRating: Array<ChallengesRating> = [];
-  public currentPage = 1;
-  public pageSize = 10;
-  public total = 0;
-
-  constructor(public service: ChallengesService) {}
-
-  ngOnInit() {
-    this.updateChallengesRating();
+  constructor(public service: ChallengesService) {
+    super();
   }
 
-  updateChallengesRating() {
-    this.service.getChallengesRating(this.currentPage, this.pageSize).subscribe(
-      (result: PageResult) => {
-        this.challengesRating = result.data;
-        this.total = result.total;
-      }
-    );
+  get challengesRatingList() {
+    return this.pageResult?.data;
+  }
+
+  ngOnInit() {
+    this.reloadPage();
+  }
+
+  getPage(): Observable<PageResult<ChallengesRating>> {
+    return this.service.getChallengesRating(this.pageNumber, this.pageSize);
   }
 
 }
