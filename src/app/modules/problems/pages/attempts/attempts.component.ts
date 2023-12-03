ee0db5f34e91bb8799faa058320304cccd0dd0dd
archon1999 <archon1999@github.com>
2023-12-03 17:09:11 +0500

    [REFACTORING] attempts page

--- src/app/modules/problems/pages/attempts/attempts.component.ts
+++ src/app/modules/problems/pages/attempts/attempts.component.ts
@@ -1,20 +1,15 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ApiService } from 'app/shared/services/api.service';
-import { User } from 'app/auth/models';
-import { asyncScheduler, interval, Subject } from 'rxjs';
-import { takeUntil, throttleTime } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { Attempt } from '@problems/models/attempts.models';
-import { GlobalService } from '@shared/services/global.service';
-import { SpinnersEnum } from '@shared/components/spinner/spinners.enum';
-import { NgxSpinnerService } from 'ngx-spinner';
 import { CoreCommonModule } from '@core/common.module';
 import { SpinnerComponent } from '@shared/components/spinner/spinner.component';
 import { AttemptsTableModule } from '@problems/components/attempts-table/attempts-table.module';
 import { KepPaginationComponent } from '@shared/components/kep-pagination/kep-pagination.component';
 import { ContentHeaderModule } from '@layout/components/content-header/content-header.module';
-
-const RELOAD_INTERVAL_TIME = 30000;
+import { BaseTablePageComponent } from '@shared/components/classes/base-table-page.component';
+import { ContentHeader } from '@layout/components/content-header/content-header.component';
+import { PageResult } from '@shared/components/classes/page-result';
+import { KepTableComponent } from '@shared/components/kep-table/kep-table.component';
 
 @Component({
   selector: 'app-attempts',
@@ -27,95 +22,45 @@ const RELOAD_INTERVAL_TIME = 30000;
     AttemptsTableModule,
     KepPaginationComponent,
     ContentHeaderModule,
+    KepTableComponent,
   ]
 })
-export class AttemptsComponent implements OnInit, OnDestroy {
-
-  public contentHeader = {
-    headerTitle: 'Attempts',
-    actionButton: true,
-    breadcrumb: {
-      type: '',
-      links: [
-        {
-          name: 'Problems',
-          isLink: true,
-          link: '/practice/problems'
-        }
-      ]
-    }
-  };
-
-  public currentPage = 1;
-  public isLoading = true;
-  public totalAttemptsCount: number;
-  public attempts: Array<Attempt> = [];
+export class AttemptsComponent extends BaseTablePageComponent<Attempt> implements OnInit, OnDestroy {
+  override maxSize = 7;
 
   public myAttempts = false;
-  public currentUser: User;
 
-  protected readonly SpinnersEnum = SpinnersEnum;
-  protected _reloader = new Subject();
-  private _unsubscribeAll = new Subject();
-  private _intervalId: any;
-
-  constructor(
-    public api: ApiService,
-    public route: ActivatedRoute,
-    public globalService: GlobalService,
-    public spinner: NgxSpinnerService,
-  ) {
+  get attempts() {
+    return this.pageResult?.data;
   }
 
   ngOnInit(): void {
-    this.globalService.currentUser$.subscribe(
-      (user: any) => {
-        this.currentUser = user;
-        if (user) {
-          this.myAttempts = true;
-          setTimeout(() => this._reloader.next(null), 100);
-        } else {
-          this.myAttempts = false;
-        }
-      }
-    );
-
-    this._reloader.pipe(
-      throttleTime(500, asyncScheduler, { leading: true, trailing: true }),
-      takeUntil(this._unsubscribeAll),
-    ).subscribe(
-      () => {
-        this._loadPage();
-      }
-    );
-
-    interval(RELOAD_INTERVAL_TIME).pipe(takeUntil(this._unsubscribeAll)).subscribe(
-      () => {
-        this._reloader.next(null);
-      }
-    );
+    this.loadContentHeader();
+    this.reloadPage();
   }
 
-  ngOnDestroy(): void {
-    if (this._intervalId) {
-      clearInterval(this._intervalId);
-    }
-    this._unsubscribeAll.next(null);
-    this._unsubscribeAll.complete();
-  }
-
-  private _loadPage() {
-    this.spinner.show(SpinnersEnum.AttemptsTable);
-    const params: any = { page: this.currentPage };
-    this.isLoading = true;
+  getPage(): Observable<PageResult<Attempt>> {
+    const params: any = { page: this.pageNumber };
     if (this.myAttempts && this.currentUser) {
       params.username = this.currentUser.username;
     }
-    this.api.get('attempts', params).subscribe((result: any) => {
-      this.attempts = result.data;
-      this.totalAttemptsCount = result.total;
-      this.isLoading = false;
-      this.spinner.hide(SpinnersEnum.AttemptsTable);
-    });
+    return this.api.get('attempts', params);
+  }
+
+  protected getContentHeader(): ContentHeader {
+    return {
+      headerTitle: 'Attempts',
+      actionButton: true,
+      breadcrumb: {
+        type: '',
+        links: [
+          {
+            name: 'Problems',
+            isLink: true,
+            link: '/practice/problems'
+          }
+        ]
+      }
+    };
   }
 }
