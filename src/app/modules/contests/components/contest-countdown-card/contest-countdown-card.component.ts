81f2e927e9a53b7a52426c588bcd455a646e464f
archon1999 <nazarbek.baltabaev@gmail.com>
2023-12-16 22:14:28 +0500

    [REFACTORING] contest-countdown-card

--- src/app/modules/contests/components/contest-countdown-card/contest-countdown-card.component.ts
+++ src/app/modules/contests/components/contest-countdown-card/contest-countdown-card.component.ts
@@ -1,11 +1,23 @@
-import { Component, Input } from '@angular/core';
-import { Contest } from '@contests/contests.models';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Contest, ContestStatus } from '@contests/contests.models';
 
 @Component({
   selector: 'contest-card-countdown',
-  templateUrl: './contest-card-countdown.component.html',
-  styleUrls: ['./contest-card-countdown.component.scss']
+  templateUrl: './contest-countdown-card.component.html',
+  styleUrls: ['./contest-countdown-card.component.scss']
 })
-export class ContestCardCountdownComponent {
+export class ContestCountdownCardComponent {
   @Input() contest: Contest;
+
+  @ViewChild('contestLogo') contestLogoRef: ElementRef<HTMLImageElement>;
+
+  public logoWidth: number;
+  public logoHeight: number;
+
+  protected readonly ContestStatus = ContestStatus;
+
+  onLoad(event: any) {
+    this.logoHeight = this.contestLogoRef.nativeElement.naturalHeight;
+    this.logoWidth = this.contestLogoRef.nativeElement.naturalWidth;
+  }
 }
