d9569cfac70d18126ac2f1d0db18210d36bff584
archon1999 <nazarbek.baltabaev@gmail.com>
2023-12-17 22:22:26 +0500

    [REFACTORING] auth service

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable, of } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 import { environment } from 'environments/environment';
 import { User } from 'app/auth/models';
@@ -10,34 +10,29 @@ import { WebsocketService } from 'app/shared/services/websocket';
 
 @Injectable({ providedIn: 'root' })
 export class AuthenticationService {
-  public currentUser: Observable<User>;
-
-  private currentUserSubject: BehaviorSubject<User>;
+  private currentUserSubject = new BehaviorSubject<User>(null);
+  public currentUser = this.currentUserSubject.asObservable();
 
   constructor(
     private api: ApiService,
     private _http: HttpClient,
     public wsService: WebsocketService,
-  ) {
-    this.currentUserSubject = new BehaviorSubject<User>(null);
-    this.currentUser = this.currentUserSubject.asObservable();
-    this.updateMe();
-  }
+  ) {}
 
   public get currentUserValue(): User {
     return this.currentUserSubject.value;
   }
 
-  get isAdmin() {
-    return this.currentUser && this.currentUserSubject.value.isSuperuser;
-  }
-
-  get isAuthenticated() {
-    return this.currentUser;
-  }
-
-  get me() {
-    return this.api.get('me');
+  getMe() {
+    return this.api.get('me').pipe(
+      tap((user: User) => {
+        if (user) {
+          this.currentUserSubject.next(user);
+        } else {
+          this.currentUserSubject.next(null);
+        }
+      })
+    );
   }
 
   login(username: string, password: string) {
@@ -45,35 +40,12 @@ export class AuthenticationService {
     const headers = { 'Authorization': `Basic ${ token }` };
     return this._http
       .post<any>(`${ environment.apiUrl }/api/login/`, {}, { headers: headers })
-      .pipe(
-        map(user => {
-          this.currentUserSubject.next(user);
-          return user;
-        })
-      );
-  }
-
-  updateMe() {
-    this.me.subscribe(
-      (user: User) => {
-        this.currentUserSubject.next(user);
-      },
-      (error: any) => {
-        this.currentUserSubject.next(null);
-      }
-    );
+      .pipe(tap(user => this.currentUserSubject.next(user)));
   }
 
   updateKepcoin(kepcoin: number) {
     if (this.currentUserValue) {
       this.currentUserValue.kepcoin = kepcoin;
-    }
-  }
-
-  checkDailyActivity() {
-    if (this.currentUserValue) {
-      this.api.get('users/check-daily-activity').subscribe((result: any) => {
-      });
     }
   }
 
