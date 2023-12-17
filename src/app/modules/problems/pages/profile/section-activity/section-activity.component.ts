5d65e0bf40471ab40ffaec677b61bd7a4804143f
archon1999 <nazarbek.baltabaev@gmail.com>
2023-12-17 17:40:29 +0500

    [REFACTORING] problems profile page

--- src/app/modules/problems/pages/profile/section-activity/section-activity.component.ts
+++ src/app/modules/problems/pages/profile/section-activity/section-activity.component.ts
@@ -1,109 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { fadeInOnEnterAnimation } from 'angular-animations';
-import { colors } from 'app/colors.const';
-import { ProblemsStatisticsService } from '../../../services/problems-statistics.service';
+import { Component, Input } from '@angular/core';
 import { CoreCommonModule } from '@core/common.module';
-import { NgbButtonsModule } from '@ng-bootstrap/ng-bootstrap';
-import { ApexChartModule } from '@shared/third-part-modules/apex-chart/apex-chart.module';
+import { ProblemsActivityCardComponent } from '@problems/components/problems-activity-card/problems-activity-card.component';
 
 @Component({
   selector: 'section-activity',
   templateUrl: './section-activity.component.html',
   styleUrls: ['./section-activity.component.scss'],
-  animations: [fadeInOnEnterAnimation({ duration: 3000 })],
   standalone: true,
   imports: [
     CoreCommonModule,
-    NgbButtonsModule,
-    ApexChartModule,
+    ProblemsActivityCardComponent,
   ]
 })
-export class SectionActivityComponent implements OnInit {
-
+export class SectionActivityComponent {
   @Input() username: string;
-
-  public activityDays = 7;
-  public activitySolved = 0;
-  public activityChart: any;
-  public solvedText: string;
-
-  public chartTheme: {
-    mode: string,
-  };
-
-  constructor(
-    public statisticsService: ProblemsStatisticsService,
-  ) { }
-
-  ngOnInit(): void {
-    this.activityDataUpdate(this.activityDays);
-  }
-
-  activityDataUpdate(days: number) {
-    this.activityDays = days;
-    const username = this.username;
-    this.statisticsService.getLastDays(username, days).subscribe((result: any) => {
-      this.activitySolved = result.solved;
-      const data = [];
-      let days = 0;
-      for (const y of result.series) {
-        const dt = new Date(Date.now() - days * 1000 * 60 * 60 * 24);
-        data.push({
-          x: dt.toDateString(),
-          y: y,
-        });
-        days++;
-      }
-      this.activityChart = {
-        chart: {
-          height: '140px',
-          type: 'line',
-          toolbar: {
-            show: false
-          },
-          sparkline: {
-            enabled: true
-          },
-          fontFamily: 'Quicksand, Raleway',
-        },
-        colors: [colors.solid.primary],
-        dataLabels: {
-          enabled: false
-        },
-        stroke: {
-          curve: 'smooth',
-          width: 5
-        },
-        fill: {
-          type: 'gradient',
-          gradient: {
-            shadeIntensity: 1,
-            gradientToColors: ['#A9A2F6'],
-            opacityFrom: 1,
-            opacityTo: 1,
-            stops: [0, 100, 100, 100]
-          }
-        },
-        yaxis: {
-          labels: {
-            show: false,
-            formatter: function (val) {
-              return val + '';
-            },
-          }
-        },
-        series: [
-          {
-            name: this.solvedText,
-            data: data,
-          }
-        ],
-        tooltip: {
-          x: { show: false }
-        },
-      };
-
-    });
-  }
-
 }
