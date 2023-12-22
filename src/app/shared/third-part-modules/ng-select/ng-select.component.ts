42ab8dfd18f38a6476d8a4f90d559f252676334c
archon1999 <archon1999@github.com>
2023-12-22 14:41:10 +0500

    [REFACTORING] ng-select

--- src/app/shared/third-part-modules/ng-select/ng-select.component.ts
+++ src/app/shared/third-part-modules/ng-select/ng-select.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @Component({
   selector: 'ng-select-css',
   templateUrl: './ng-select.component.html',
   styleUrls: ['./ng-select.component.scss'],
   encapsulation: ViewEncapsulation.None,
+  standalone: true,
+  imports: [
+    NgSelectModule,
+  ]
 })
 export class NgSelectComponent implements OnInit {
 
