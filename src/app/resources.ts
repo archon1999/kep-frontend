5fa937a37360eb3ba3e0a033720e70c398f49de2
archon1999 <nazarbek.baltabaev@gmail.com>
2024-01-03 10:11:13 +0500

    [ADD] new resources & getResourceById

--- src/app/resources.ts
+++ src/app/resources.ts
@@ -1,3 +1,13 @@
 export enum Resources {
   Problems = '/practice/problems',
+  Problem = '/practice/problems/problem/:id',
+
+  Attempts = '/practice/problems/attempts',
+
+  Contests = '/competitions/contests',
+  Contest = '/competitions/contests/contest/:id',
+}
+
+export function getResourceById(resource: Resources, id: number | string) {
+  return resource.replace(':id', id.toString());
 }
