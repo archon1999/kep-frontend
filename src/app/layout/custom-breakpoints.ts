aae3baa29a6230827c3b77dc62c104a611b150f3
archon1999 <nazarbek.baltabaev@gmail.com>
2024-01-03 10:10:04 +0500

    [REFACTORING] custom breakpoints

--- src/app/layout/custom-breakpoints.ts
+++ src/app/layout/custom-breakpoints.ts
@@ -1,21 +1,21 @@
 import { BREAKPOINT } from '@angular/flex-layout';
 
-const BS_BREAKPOINTS = [
-  { alias: 'bs-xs', overlapping: false, mediaQuery: '(max-width: 575.98px)', suffix: 'BsXs' },
-  { alias: 'bs-sm', overlapping: false, mediaQuery: '(min-width: 576px) and (max-width: 767.98px)', suffix: 'BsSm' },
-  { alias: 'bs-md', overlapping: false, mediaQuery: '(min-width: 768px) and (max-width: 991.98px)', suffix: 'BsMd' },
-  { alias: 'bs-lg', overlapping: false, mediaQuery: '(min-width: 992px) and (max-width: 1199.98px)', suffix: 'BsLg' },
-  { alias: 'bs-xl', overlapping: false, mediaQuery: '(min-width: 1200px)', suffix: 'BsXl' },
+export const BS_BREAKPOINTS = [
+  { alias: 'xs', overlapping: false, mediaQuery: '(max-width: 575.98px)' },
+  { alias: 'sm', overlapping: false, mediaQuery: '(min-width: 576px) and (max-width: 767.98px)', priority: 1001 },
+  { alias: 'md', overlapping: false, mediaQuery: '(min-width: 768px) and (max-width: 991.98px)', priority: 1001 },
+  { alias: 'lg', overlapping: false, mediaQuery: '(min-width: 992px) and (max-width: 1199.98px)', priority: 1001 },
+  { alias: 'xl', overlapping: false, mediaQuery: '(min-width: 1200px)', priority: 1001 },
 
-  { alias: 'bs-gt-sm', overlapping: false, mediaQuery: '(min-width: 576px)', suffix: 'BsGtSm' },
-  { alias: 'bs-gt-md', overlapping: false, mediaQuery: '(min-width: 768px)', suffix: 'BsGtMd' },
-  { alias: 'bs-gt-lg', overlapping: false, mediaQuery: '(min-width: 992px)', suffix: 'BsGtLg' },
-  { alias: 'bs-gt-xl', overlapping: false, mediaQuery: '(min-width: 1200px)', suffix: 'BsGtXl' },
+  { alias: 'gt-sm', overlapping: false, mediaQuery: '(min-width: 576px)', priority: 1001 },
+  { alias: 'gt-md', overlapping: false, mediaQuery: '(min-width: 768px)', priority: 1001 },
+  { alias: 'gt-lg', overlapping: false, mediaQuery: '(min-width: 992px)', priority: 1001 },
+  { alias: 'gt-xl', overlapping: false, mediaQuery: '(min-width: 1200px)', priority: 1001 },
 
-  { alias: 'bs-lt-sm', overlapping: false, mediaQuery: '(max-width: 575.98px)', suffix: 'BsLtSm' },
-  { alias: 'bs-lt-md', overlapping: false, mediaQuery: '(max-width: 767.98px)', suffix: 'BsLtMd' },
-  { alias: 'bs-lt-lg', overlapping: false, mediaQuery: '(max-width: 991.98px)', suffix: 'BsLtLg' },
-  { alias: 'bs-lt-xl', overlapping: false, mediaQuery: '(max-width: 1199.98px)', suffix: 'BsLtXl' }
+  { alias: 'lt-sm', overlapping: false, mediaQuery: '(max-width: 575.98px)', priority: 1001 },
+  { alias: 'lt-md', overlapping: false, mediaQuery: '(max-width: 767.98px)', priority: 1001 },
+  { alias: 'lt-lg', overlapping: false, mediaQuery: '(max-width: 991.98px)', priority: 1001 },
+  { alias: 'lt-xl', overlapping: false, mediaQuery: '(max-width: 1199.98px)', priority: 1001 }
 ];
 
 export const CustomBreakPointsProvider = {
