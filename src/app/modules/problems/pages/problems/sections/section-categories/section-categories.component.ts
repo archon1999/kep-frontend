415f46f47108bd64e1a44d2e595f8ae14f0c6f2c
archon1999 <nazarbek.baltabaev@gmail.com>
2024-01-03 10:07:22 +0500

    [ADD] new sections | problems

---
+++ src/app/modules/problems/pages/problems/sections/section-categories/section-categories.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { ProblemsApiService } from '@problems/services/problems-api.service';
+import { Category } from '@problems/models/problems.models';
+import { CoreCommonModule } from '@core/common.module';
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { fadeInOnEnterAnimation } from 'angular-animations';
+import { BaseLoadComponent } from '@shared/components/classes/base-load.component';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'section-categories',
+  standalone: true,
+  imports: [CoreCommonModule, NgbTooltipModule],
+  templateUrl: './section-categories.component.html',
+  styleUrl: './section-categories.component.scss',
+  animations: [fadeInOnEnterAnimation()]
+})
+export class SectionCategoriesComponent extends BaseLoadComponent<Array<Category>> {
+
+  public showAll = true;
+
+  constructor(
+    public service: ProblemsApiService
+  ) {
+    super();
+  }
+
+  get categories() {
+    return this.data;
+  }
+
+  getData(): Observable<Array<Category>> | null {
+    return this.service.getCategories();
+  }
+
+}
---
+++ src/app/modules/problems/pages/problems/sections/section-problems-list/section-problems-list.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { SectionProblemsTableComponent } from '../section-problems-table/section-problems-table.component';
+import { CoreCommonModule } from '@core/common.module';
+import { ProblemCardComponent } from '@problems/pages/problems/sections/section-problems-list/problem-card/problem-card.component';
+import { KepPaginationComponent } from '@shared/components/kep-pagination/kep-pagination.component';
+
+@Component({
+  selector: 'section-problems-list',
+  standalone: true,
+  imports: [
+    CoreCommonModule,
+    ProblemCardComponent,
+    KepPaginationComponent,
+  ],
+  templateUrl: './section-problems-list.component.html',
+  styleUrl: './section-problems-list.component.scss'
+})
+export class SectionProblemsListComponent extends SectionProblemsTableComponent {
+  override defaultPageSize = 12;
+  override pageOptions = [6, 12, 24, 36];
+}
