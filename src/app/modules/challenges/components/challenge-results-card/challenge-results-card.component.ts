0af98410219fd307157562330fd3c37e62ad02be
archon1999 <archon1999@github.com>
2024-01-08 11:06:29 +0500

    [REFACTORING] challenges components | make standalone

--- src/app/modules/challenges/components/challenge-results-card/challenge-results-card.component.ts
+++ src/app/modules/challenges/components/challenge-results-card/challenge-results-card.component.ts
@@ -1,18 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Challenge } from '../../models/challenges.models';
+import { CoreCommonModule } from '@core/common.module';
+import { ChallengesUserViewModule } from '@challenges/components/challenges-user-view/challenges-user-view.module';
 
 @Component({
   selector: 'challenge-results-card',
   templateUrl: './challenge-results-card.component.html',
-  styleUrls: ['./challenge-results-card.component.scss']
+  styleUrls: ['./challenge-results-card.component.scss'],
+  standalone: true,
+  imports: [
+    CoreCommonModule,
+    ChallengesUserViewModule,
+  ]
 })
-export class ChallengeResultsCardComponent implements OnInit {
-
+export class ChallengeResultsCardComponent {
   @Input() challenge: Challenge;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
