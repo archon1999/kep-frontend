62f1bf4f2643cc757a7558ab2abb08d8af618f77
archon1999 <nazarbek.baltabaev@gmail.com>
2024-01-08 11:01:53 +0500

    [ADD] base-user component

---
+++ src/app/shared/components/classes/base-user.component.ts
@@ -0,0 +1,31 @@
+import { Component, inject, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { User } from '@auth/models';
+import { AuthService } from '@auth/service';
+import { takeUntil } from 'rxjs/operators';
+
+@Component({
+  template: '',
+  standalone: true
+})
+export class BaseUserComponent implements OnDestroy {
+  public currentUser: User;
+  protected authService = inject(AuthService);
+  private _unsubscribeAll = new Subject();
+
+  constructor() {
+    this.authService.currentUser.pipe(takeUntil(this._unsubscribeAll)).subscribe(
+      (user: User | null) => {
+        this.currentUser = user;
+        this.afterChangeCurrentUser(user);
+      }
+    );
+  }
+
+  afterChangeCurrentUser(currentUser: User) {}
+
+  ngOnDestroy() {
+    this._unsubscribeAll.next(null);
+    this._unsubscribeAll.complete();
+  }
+}
