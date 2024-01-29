8d69be1e8117ff1a72cc8eb09efa767832f61ee6
archon1999 <archon1999@github.com>
2024-01-29 15:07:09 +0500

    [REFACTORING] contests page

--- src/app/modules/contests/pages/contests/sections/section-filter/section-filter.component.ts
+++ src/app/modules/contests/pages/contests/sections/section-filter/section-filter.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CoreCommonModule } from '@core/common.module';
 
 @Component({
   selector: 'section-filter',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CoreCommonModule],
   templateUrl: './section-filter.component.html',
   styleUrl: './section-filter.component.scss'
 })
