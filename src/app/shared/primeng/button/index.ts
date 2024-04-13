3fdf29a15fe023bb3b9b6afb9313f52fdced1f19
archon1999 <nazarbek.baltabaev@gmail.com>
2024-04-13 09:57:37 +0500

    [ADD] primeng

---
+++ src/app/shared/primeng/button/index.ts
@@ -0,0 +1,4 @@
+export { ButtonTemplates } from './button.interface';
+export { ButtonComponent } from './button.component';
+export { ButtonDirective } from './button.directive';
+export { ButtonModule } from './button.module';
