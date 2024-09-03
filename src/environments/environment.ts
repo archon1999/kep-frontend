12becb76d8f658b5e1f0bda3298b618ed99c684e
archon1999 <nazarbek.baltabaev@gmail.com>
2024-09-04 00:25:54 +0500

    [CHANGE] apiUrl & wsUrl for local env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   hmr: false,
-  apiUrl: 'http://193.164.16.215:8008',
-  wsUrl: 'ws://193.164.16.215:8443/ws',
+  apiUrl: 'http://193.164.17.34:8008',
+  wsUrl: 'ws://193.164.17.34:8443/ws',
   superAdmin: {
    username: 'admin',
    password: 'CPython.uz',
