26a6af250fb9fd76e4aa32542be5df4509603970
archon1999 <nazarbek.baltabaev@gmail.com>
2024-12-09 15:31:30 +0500

    Archa bayram

--- src/@core/layout/layout.component.ts
+++ src/@core/layout/layout.component.ts
@@ -1,33 +1,83 @@
-import { ChangeDetectorRef, Component, ViewEncapsulation } from '@angular/core';
-
+import { ChangeDetectorRef, Component, HostListener, Renderer2, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HorizontalLayoutComponent } from '@layout/horizontal/horizontal-layout.component';
 import { VerticalLayoutComponent } from '@layout/vertical/vertical-layout.component';
 import { CoreConfigService } from '@core/services/config.service';
 import { CoreConfig } from '@core/types';
-import { FooterModule } from '@layout/components/footer/footer.module';
-import { NgTemplateOutlet } from '@angular/common';
+import { ScriptService } from '@shared/services/script.service';
+import { randomInt } from '@shared/utils';
+import { LocalStorageService } from '@shared/services/storages/local-storage.service';
 
+const SCRIPT_PATH = 'assets/plugins/snowf/snowf.min.js';
+let snowf: any;
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HorizontalLayoutComponent, VerticalLayoutComponent, FooterModule, NgTemplateOutlet],
+  imports: [CommonModule, HorizontalLayoutComponent, VerticalLayoutComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
   encapsulation: ViewEncapsulation.None
 })
 export class LayoutComponent {
   public coreConfig: CoreConfig;
+  public lightsCount = 0;
+  public lightsEnabled: boolean = !!this.localStorageService.get('lightsEnabled') || true;
 
   constructor(
     public coreConfigService: CoreConfigService,
+    private renderer: Renderer2,
+    private scriptService: ScriptService,
     public changeDetection: ChangeDetectorRef,
+    public localStorageService: LocalStorageService,
   ) {
+    this.lightsCount = Math.trunc(window.innerHeight / 30);
+
     coreConfigService.getConfig().subscribe(
       (config) => {
         this.coreConfig = config;
         this.changeDetection.markForCheck();
       }
     );
+
+    const scriptElement = this.scriptService.loadJsScript(this.renderer, SCRIPT_PATH);
+    scriptElement.onload = (e) => {
+      snowf = window['snowf'].init({
+        size: 2,
+        amount: 20,
+        speed: 1
+      });
+      window.onresize = function () {
+        snowf.resize();
+      };
+      setTimeout(() => {
+        snowf.setOptions({
+          size: 5,
+          amount: 50,
+          speed: 1.5
+        });
+        setTimeout(() => {
+          snowf.setOptions({
+            size: 7,
+            amount: 75,
+            speed: 2,
+            wind: 1,
+          });
+        }, 10 * 60 * 1000);
+      }, 30000);
+      setInterval(() => {
+        snowf.wind(randomInt(1, 4), randomInt(1000, 5000));
+      }, 100 * 1000);
+    };
+  }
+
+  updateLightsEnabled() {
+    this.lightsEnabled = !this.lightsEnabled;
+    this.localStorageService.set('lightsEnabled', this.lightsEnabled);
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.lightsCount = Math.trunc(window.innerHeight / 30);
   }
 }
