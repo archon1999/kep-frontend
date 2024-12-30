3b4ab543ac4a51c54017d0f0755cf98c76f38d73
archon1999 <nazarbek.baltabaev@gmail.com>
2024-12-31 00:24:41 +0500

    refactoring new year effects

--- src/app/shared/components/christmas-tree/christmas-tree.component.ts
+++ src/app/shared/components/christmas-tree/christmas-tree.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { LocalStorageService } from '@shared/services/storages/local-storage.service';
 
 @Component({
   selector: 'christmas-tree',
@@ -7,6 +8,11 @@ import { Component } from '@angular/core';
   templateUrl: './christmas-tree.component.html',
   styleUrl: './christmas-tree.component.scss'
 })
-export class ChristmasTreeComponent {
+export class ChristmasTreeComponent implements OnInit {
+  public newYearEffects: boolean;
+  protected localStorageService = inject(LocalStorageService);
 
+  ngOnInit() {
+    this.newYearEffects = this.localStorageService.get('newYearEffects', true);
+  }
 }
