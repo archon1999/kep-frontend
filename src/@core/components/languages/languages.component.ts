6bebc9886b344431f1815b0e27406b211a603ae0
archon1999 <nazarbek.baltabaev@gmail.com>
2025-03-01 15:12:07 +0500

    language update fix

--- src/@core/components/languages/languages.component.ts
+++ src/@core/components/languages/languages.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { NgbDropdown, NgbDropdownMenu, NgbDropdownToggle } from "@ng-bootstrap/ng-bootstrap";
 import { AppStateService, Language } from "@core/services/app-state.service";
+import { ApiService } from '@shared/services/api.service';
 
 @Component({
   selector: 'languages',
@@ -31,6 +32,7 @@ export class LanguagesComponent {
   public currentLang: Language;
 
   protected appStateService = inject(AppStateService);
+  protected api = inject(ApiService);
 
   constructor() {
     this.appStateService.state$.subscribe(
@@ -41,8 +43,10 @@ export class LanguagesComponent {
   }
 
   setLanguage(lang: Language) {
-    this.appStateService.updateState({language: lang});
-    location.reload();
+    this.api.post('set-language/', { language: lang }).subscribe(() => {
+      this.appStateService.updateState({language: lang});
+      location.reload();
+    })
   }
 
   protected readonly Object = Object;
