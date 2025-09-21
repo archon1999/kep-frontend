f579b3fed07668beca1d9a09654823148fb8b9ea
Nazarbek Baltabaev <63235428+archon1999@users.noreply.github.com>
2025-09-22 00:22:44 +0500

    feat: add duels listing page

--- src/app/modules/duels/duels.interfaces.ts
+++ src/app/modules/duels/duels.interfaces.ts
@@ -24,7 +24,53 @@ export interface Duel {
   finishTime: Date;
   status: DuelStatus;
   isPlayer: boolean;
+  isConfirmed?: boolean;
   playerFirst: DuelPlayer;
-  playerSecond: DuelPlayer;
+  playerSecond: DuelPlayer | null;
   problems?: Array<DuelProblem>;
+}
+
+export interface DuelReadyPlayer {
+  username: string;
+  fullName: string;
+  avatar: string;
+  wins: number;
+  draws: number;
+  losses: number;
+}
+
+export interface DuelPresetProblem {
+  id: number;
+  symbol: string;
+  ball: number;
+}
+
+export interface DuelPresetTypeInfo {
+  id: number;
+  code: string;
+  title: string;
+  description: string;
+}
+
+export interface DuelPresetCategory {
+  id: number;
+  title: string;
+}
+
+export interface DuelPreset {
+  id: number;
+  type: string;
+  typeInfo: DuelPresetTypeInfo;
+  difficulty: number;
+  difficultyDisplay: string;
+  title: string;
+  description: string;
+  duration: string;
+  category: DuelPresetCategory;
+  problemsCount: number;
+  problems: Array<DuelPresetProblem>;
+}
+
+export interface DuelReadyStatus {
+  ready: boolean;
 }
