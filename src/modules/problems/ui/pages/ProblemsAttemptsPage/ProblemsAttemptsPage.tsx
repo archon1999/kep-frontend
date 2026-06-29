f14ea83b418b3dcbc4aa517746b293ade1cb7df5
Nazarbek Baltabaev <nazarbek.baltabaev@gmail.com>
2026-06-29 16:16:34 +0500

    fix

--- src/modules/problems/ui/pages/ProblemsAttemptsPage/ProblemsAttemptsPage.tsx
+++ src/modules/problems/ui/pages/ProblemsAttemptsPage/ProblemsAttemptsPage.tsx
@@ -340,7 +340,7 @@ const ProblemsAttemptsPage = () => {
           }
         />
 
-        <Box sx={{ flex: 1, px: { xs: 2, sm: 3, md: 5 }} }}>
+        <Box sx={{ flex: 1, px: { xs: 2, sm: 3, md: 5 }}}>
           <ProblemsAttemptsTable
             attempts={attempts}
             total={total}
